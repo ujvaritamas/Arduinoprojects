0c272f036e79ce6d2b603df22d41a2a2e00b87e8
ujvaritamas <49878381+ujvaritamas@users.noreply.github.com>
2023-08-17 10:50:08 +0200

    correction

--- builtin_led_blinking_low_level/my_blinking.cpp
+++ builtin_led_blinking_low_level/my_blinking.cpp
@@ -1,23 +1,22 @@
 #include "my_blinking.h"
-#include <Arduino_h>
+#include <Arduino.h>
 
 void init_builtin_led(){
-    volatile uint8_t* p;
+    volatile uint8_t* p = (uint8_t*) MY_DDRB_ADDR;
 
     //configure PB5 to output
-    p = MY_DDRB_ADDR;
     *p |= 1<<5;
     //*(MY_DDRB_ADDR) |= 1<<5;
 }
 
 void turn_on_builtin_led(){
-    volatile uint8_t* p;
+    volatile uint8_t* p = (uint8_t*) MY_PORTB_ADDR;
     p = MY_PORTB_ADDR;
     *p |= 1 << 5;
 }
 
 void turn_off_builtin_led(){
-    volatile uint8_t* p;
+    volatile uint8_t* p = (uint8_t*) MY_PORTB_ADDR;
     p = MY_PORTB_ADDR;
     *p &= ~(1 << 5);
 }
